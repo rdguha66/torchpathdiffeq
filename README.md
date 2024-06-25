290dc5d8f6809fd03b65fca8117da5db3a526fe2
khegazy <KareemHegz@gmail.com>
2024-06-24 21:53:14 -0700

    Changed template folder to torchpathdiffeq

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# template
+# torchpathdiffeq
 
 A simple-to-use resource for creating open-source Python packages.
--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -12,9 +12,9 @@ import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("src").rglob("*.py")):
-    module_path = path.relative_to("src").with_suffix("")
-    doc_path = path.relative_to("src").with_suffix(".md")
+for path in sorted(Path("torchpathdiffeq").rglob("*.py")):
+    module_path = path.relative_to("torchpathdiffeq").with_suffix("")
+    doc_path = path.relative_to("torchpathdiffeq").with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
     parts = tuple(module_path.parts)
